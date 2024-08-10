5a517d000fad946dd95050d6c793044f142ff926
sixteenis <psm0103907@naver.com>
2024-08-10 13:48:32 +0900

    초기 setUp

---
+++ AppstoreProject/Base/BaseView.swift
@@ -0,0 +1,27 @@
+//
+//  BaseView.swift
+//  MeaningOutProject
+//
+//  Created by 박성민 on 7/23/24.
+//
+import UIKit
+
+class BaseView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpHierarchy()
+        setUpLayout()
+        setUpView()
+        self.backgroundColor = .white
+    }
+    func setUpHierarchy() { }
+    func setUpLayout() { }
+    func setUpView() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
---
+++ AppstoreProject/Base/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  MeaningOutProject
+//
+//  Created by 박성민 on 7/23/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        setUpHierarchy()
+        setUpView()
+        setUpLayout()
+        bindData()
+        setUpNavLeft()
+    }
+    
+    func setUpHierarchy() {}
+    func setUpLayout() {}
+    func setUpView() {}
+    func bindData() {}
+    func setUpNavLeft() {
+        self.navigationController?.navigationBar.topItem?.title = ""
+        navigationController?.navigationBar.tintColor = .black
+    }
+}
